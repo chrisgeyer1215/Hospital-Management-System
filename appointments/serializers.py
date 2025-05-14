6bd9cf048b48baa1d2a8c03a89f3a0a39497961c
Christopher Geyer <chrisgeyer1215.dev@outlook.com>
2025-05-14 05:45:13 -0300

    Update serializers.py
    ceb

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from .models import PatientAppointment,UserMessages
+from patients.models import Patient,TestResult
+from doctors.models import Doctors, TimeSlot
+from doctors.serializers import TimeSlotSerializer
 
 
+class DoctorsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Doctors
+        fields='__all__'
 
-class PatientAppointmentSerializer(serializers.ModelSerializer):
+class TestResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=TestResult
+        fields='__all__'
+
+class PatientSerializer(serializers.ModelSerializer):
+    
+    test_results = TestResultSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model=Patient
+        fields=['id','full_name','phone','age','gender','blood_type','emergency_contact','test_results']
+        
+
+class Patientnew_appointmentserializer(serializers.ModelSerializer):
+    patient=PatientSerializer()
+    timeslot=TimeSlotSerializer()
+    
+
     class Meta:
         model=PatientAppointment
         fields = '__all__'
