a76eda1e9dea8a83d47dfd91e9279e3da8d257b5
Christopher Geyer <chrisgeyer1215.dev@outlook.com>
2025-05-14 05:46:04 -0300

    Update urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,6 +4,8 @@ from .views import PatientAppointmentCreateView,PatientAppointmentListView
 
 urlpatterns = [
     path('list/', views.PatientAppointmentListView.as_view(), name='appointment-list'),
+    path('all/',views.PatientAppointmentAllListView.as_view(),name='all_appointment_based_on_doctor'),
     path('create/', views.PatientAppointmentCreateView.as_view(), name='appointment-create'),
+    path('<int:id>/status/',views.UpdatePatientAppointmentView.as_view(),name='update-status'),
     path('message/',views.UserMessagesCreateView.as_view(),name="user_message")
 ]
